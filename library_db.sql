ae8d970f6d66ffa9d98e07a2833c58902258a0d0
subasree D <sreesuba219.2005@gmail.com>
2025-12-14 12:45:15 +0530

    Add files via upload

---
+++ library_db.sql
@@ -0,0 +1,7 @@
+USE library_db;
+
+INSERT INTO books (title, author, issued) VALUES
+('Java Basics', 'James', 0),
+('C Programming', 'Dennis Ritchie', 0),
+('Python Basics', 'Guido', 0);
+SELECT * FROM books;
